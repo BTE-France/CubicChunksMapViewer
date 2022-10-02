75f66c0bc1f70613719fe7d38edbe01f2e98ee5a
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2022-10-03 00:37:38 +0100

    oops

--- launch.py
+++ launch.py
@@ -2,7 +2,7 @@ import subprocess
 import time
 
 start = time.time()
-subprocess.run("python.exe overviewer.py --config=config.py --forcerender".split())
-subprocess.run("python.exe overviewer.py --config=config.py --genpoi --skip-players --skip-scan".split())
+subprocess.run("sudo python3.9 overviewer.py --config=config.py --forcerender".split())
+subprocess.run("sudo python3.9 overviewer.py --config=config.py --genpoi --skip-players --skip-scan".split())
 end = time.time()
 print(f"Map was succesfully generated in {time.strftime('%H hour(s), %M minute(s) and %S second(s)', time.gmtime(end - start))}")
