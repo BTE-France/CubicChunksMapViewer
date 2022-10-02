0b1ea8695e34b958b13c353457f1f64774663af6
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2022-10-02 12:47:42 +0100

    create output folder and copy icons

--- config.py
+++ config.py
@@ -1,31 +1,33 @@
 from typing import TYPE_CHECKING
 from importlib import import_module
+import shutil
 import os
 
 if TYPE_CHECKING:
     from bte_france.classes import Region
 
 
-def poiFilter(poi):
-    if poi["id"]:
-        return poi['name']
+worlds["BTEFrance"] = "./worlds/CCMap"  # noqa
+outputdir = "./outputs/BTEFranceMap"
 
+# Create output directory
+if not os.path.exists(outputdir):
+    os.makedirs(outputdir)
 
-worlds["BTEFrance"] = "./worlds/CCMap"
+# Copy icons to their appropriate directory
+shutil.copytree("./bte_france/icons", f"{outputdir}/icons", dirs_exist_ok=True)
 
 regions: list["Region"] = []
 for file in os.listdir("./bte_france/regions"):
     if file.endswith(".py"):
         regions.append(getattr(import_module(f"bte_france.regions.{file[:-3]}"), "REGION"))
 
-renders["BTEFrance"] = {
+renders["BTEFrance"] = {  # noqa
     "world": "BTEFrance",
     "title": "BTEFrance",
-    "rendermode": normal,
+    "rendermode": normal,  # noqa
     "showspawn": False,
     "BTEcrop": [location.tuple for region in regions for location in region.locations],
     "manualpois": [marker.dict for region in regions for marker in region.markers],
     "markers": [region.markers_dict for region in regions]
 }
-
-outputdir = "./outputs/BTEFranceMap"
