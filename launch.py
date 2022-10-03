1c27db06ce9d7698816a5a4d9e7453d52b336271
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2022-10-04 00:58:59 +0100

    add error handling for launch script

--- launch.py
+++ launch.py
@@ -1,8 +1,13 @@
 import subprocess
 import time
 
-start = time.time()
-subprocess.run("sudo python3.9 overviewer.py --config=config.py --forcerender".split())
-subprocess.run("sudo python3.9 overviewer.py --config=config.py --genpoi --skip-players --skip-scan".split())
-end = time.time()
-print(f"Map was succesfully generated in {time.strftime('%H hour(s), %M minute(s) and %S second(s)', time.gmtime(end - start))}")
+try:
+    start = time.time()
+    subprocess.run("sudo python3.9 overviewer.py --config=config.py --forcerender".split(), check=True)
+    subprocess.run("sudo python3.9 overviewer.py --config=config.py --genpoi --skip-players --skip-scan".split(), check=True)
+    end = time.time()
+    print(f"Map was succesfully generated in {time.strftime('%H hour(s), %M minute(s) and %S second(s)', time.gmtime(end - start))}")
+except subprocess.CalledProcessError:
+    print("An error was encountered while generating the map!")
+except KeyboardInterrupt:
+    print("Map generation manually halted.")
