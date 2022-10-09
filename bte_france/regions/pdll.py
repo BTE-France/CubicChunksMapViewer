cac566f3f233a2e449c2af8ac2bf3580e4856a23
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2022-10-09 22:29:04 +0100

    add Le Puy

--- bte_france/regions/pdll.py
+++ bte_france/regions/pdll.py
@@ -1,12 +1,12 @@
-from ..classes import Region, Marker
+from ..classes import Region, Marker, SquareLocation
 
 REGION = Region(
     id="pdll",
     name="Pays de la Loire",
     locations=[
-
+        SquareLocation(x1=2591208, z1=-4926828, x2=2592287, z2=-4925960)
     ],
     markers=[
-
+        Marker(name="Le Puy-Notre-Dame", x=2591934, y=97, z=-4926277),
     ]
 )
