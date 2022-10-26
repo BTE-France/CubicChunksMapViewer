9fb97b6be031dbe5ce6790413a4eabde9e73254e
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2022-10-26 12:37:03 +0100

    remove marker generation from timer

--- launch.py
+++ launch.py
@@ -4,8 +4,8 @@ import time
 try:
     start = time.time()
     subprocess.run("sudo python3.9 overviewer.py --config=config.py --forcerender".split(), check=True)
-    subprocess.run("sudo python3.9 overviewer.py --config=config.py --genpoi --skip-players --skip-scan".split(), check=True)
     end = time.time()
+    subprocess.run("sudo python3.9 overviewer.py --config=config.py --genpoi --skip-players --skip-scan".split(), check=True)
     print(f"Map was succesfully generated in {time.strftime('%H hour(s), %M minute(s) and %S second(s)', time.gmtime(end - start))}")
 except subprocess.CalledProcessError:
     print("An error was encountered while generating the map!")
