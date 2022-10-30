3401c3adedab1e9f838a46768144c9906c61e8ab
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2022-10-30 12:07:05 +0000

    check for OS type and use correct python executable

--- launch.py
+++ launch.py
@@ -1,11 +1,14 @@
 import subprocess
 import time
+import os
+
+PYTHON_EXECUTABLE = "python3.9" if os.name == "posix" else "python.exe"
 
 try:
     start = time.time()
-    subprocess.run("sudo python3.9 overviewer.py --config=config.py --forcerender".split(), check=True)
+    subprocess.run(f"{PYTHON_EXECUTABLE} overviewer.py --config=config.py --forcerender".split(), check=True)
     end = time.time()
-    subprocess.run("sudo python3.9 overviewer.py --config=config.py --genpoi --skip-players --skip-scan".split(), check=True)
+    subprocess.run(f"{PYTHON_EXECUTABLE} overviewer.py --config=config.py --genpoi --skip-players --skip-scan".split(), check=True)
     print(f"Map was succesfully generated in {time.strftime('%H hour(s), %M minute(s) and %S second(s)', time.gmtime(end - start))}")
 except subprocess.CalledProcessError:
     print("An error was encountered while generating the map!")
