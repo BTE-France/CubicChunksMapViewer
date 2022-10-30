62e19bac1c60b3f615e86614c220fb87f27881aa
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2022-10-30 12:39:44 +0000

    add settings file

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 from bte_france.worlds import WORLDS
 import shutil
+import json
 import os
 
 
-inputdir = "./worlds/CCMap"
-outputdir = "./outputs/BTEFranceMap"
+with open("./settings.json", "r") as f:
+    settings = json.load(f)
+    inputdir = settings["WORLD_INPUT"]
+    outputdir = settings["WORLD_OUTPUT"]
 
 # Create output directory
 if not os.path.exists(outputdir):
