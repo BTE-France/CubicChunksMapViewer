1921a4f61b9934f4e059bef83992a2b601b1dad8
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2022-10-31 15:27:08 +0000

    revert to a single world configuration, and add zoom levels to allow to see Islands

--- bte_france/regions/reu.py
+++ bte_france/regions/reu.py
@@ -3,7 +3,7 @@ from ..classes import Region, Marker, SquareLocation, Type
 REGION = Region(
     id="reu",
     name="La Réunion",
-    prefecture=Marker(name="La Réunion - Saint-Denis", x=-6490371, y=7, z=-2732600),
+    prefecture=Marker(name="La Réunion - Saint-Denis", x=10619889, y=7, z=3133503),
     locations=[
         SquareLocation(x1=10617478, z1=3133919, x2=10617879, z2=3134450),  # St-Denis
     ],
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-from bte_france.worlds import WORLDS
+from bte_france.classes import Marker, Region, generate_borders
+from importlib import import_module
 import shutil
 import json
 import os
@@ -6,7 +7,7 @@ import os
 
 with open("./settings.json", "r") as f:
     settings = json.load(f)
-    inputdir = settings["WORLD_INPUT"]
+    worlds["BTE France"] = settings["WORLD_INPUT"]  # noqa
     outputdir = settings["WORLD_OUTPUT"]
 
 # Create output directory
@@ -16,6 +17,33 @@ if not os.path.exists(outputdir):
 # Copy icons to their appropriate directory
 shutil.copytree("./bte_france/icons", f"{outputdir}/icons", dirs_exist_ok=True)
 
-for world in WORLDS:
-    worlds[world.name] = inputdir  # noqa
-    renders[world.id] = world.dict  # noqa
+regions: list["Region"] = []
+for file in os.listdir("./bte_france/regions"):
+    if file.endswith(".py"):
+        regions.append(getattr(import_module(f"bte_france.regions.{file[:-3]}"), "REGION"))
+
+BTEcrop, manualpois, markers = [], [], [Marker.prefecture_dict]
+for region in regions:
+    manualpois.append(region.prefecture_dict)
+
+    for location in region.locations:
+        BTEcrop.append(location.tuple)
+
+    for marker in region.markers:
+        manualpois.append(marker.poi_dict)
+        markers.append(marker.marker_dict)
+markers.sort(key=lambda marker_dict: marker_dict["name"])
+
+renders["BTEFrance"] = {  # noqa
+    "world": "BTE France",
+    "title": "BTE France",
+    "rendermode": "normal",
+    "showspawn": False,
+    "minzoom": 2,
+    "defaultzoom": 6,
+    "showlocationmarker": False,
+    "center": [2793842, -4798093],
+    "BTEcrop": BTEcrop,
+    "manualpois": manualpois + generate_borders(),
+    "markers": markers
+}
