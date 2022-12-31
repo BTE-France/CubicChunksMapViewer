e912a0277200d26355f4b954af2a63444c8e58e6
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2022-12-31 17:54:41 +0100

    retry with previous gen option?

--- launch.py
+++ launch.py
@@ -6,7 +6,7 @@ PYTHON_EXECUTABLE = "python3.9" if os.name == "posix" else "python.exe"
 
 try:
     start = time.time()
-    subprocess.run(f"{PYTHON_EXECUTABLE} overviewer.py --config=config.py --check-tiles".split(), check=True)
+    subprocess.run(f"{PYTHON_EXECUTABLE} overviewer.py --config=config.py --forcerender".split(), check=True)
     end = time.time()
     subprocess.run(f"{PYTHON_EXECUTABLE} overviewer.py --config=config.py --genpoi --skip-players --skip-scan".split(), check=True)
     print(f"Map was succesfully generated in {time.strftime('%H hour(s), %M minute(s) and %S second(s)', time.gmtime(end - start))}")
